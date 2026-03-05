43cfb1ac7497020bcdfd033932ca97130bf471dd
Lucas Godoy <lucasgodoyjor@gmail.com>
2026-03-05 12:19:43 -0300

    Adicionado bloco try catch

--- 02 - Pagamento de colaboradores/Pagamento.java
+++ 02 - Pagamento de colaboradores/Pagamento.java
@@ -5,30 +5,30 @@ class Pagamento{
         double salario, pagamentoAtual, horas, total;
         total = 0;
 
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Salário por hora do colaborador 1, horas trabalhadas do colaborador 1");
-        salario = scanner.nextDouble();
-        horas = scanner.nextDouble();
-        pagamentoAtual=salario*horas;
-        System.out.println(pagamentoAtual);
-        
-        total+=pagamentoAtual;
-
-        System.out.println("Salário por hora do colaborador 2, horas trabalhadas do colaborador 2");
-        salario = scanner.nextDouble();
-        horas = scanner.nextDouble();
-        pagamentoAtual=salario*horas;
-        System.out.println(pagamentoAtual);
-        total+=pagamentoAtual;
-
-        System.out.println("Salário por hora do colaborador 3, horas trabalhadas do colaborador 3");
-        salario = scanner.nextDouble();
-        horas = scanner.nextDouble();
-        pagamentoAtual=salario*horas;
-        System.out.println(pagamentoAtual);
-
-        total+=pagamentoAtual;
-        System.out.println("Total: "+ total);
-        scanner.close();
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.println("Salário por hora do colaborador 1, horas trabalhadas do colaborador 1");
+            salario = scanner.nextDouble();
+            horas = scanner.nextDouble();
+            pagamentoAtual=salario*horas;
+            System.out.println(pagamentoAtual);
+            
+            total+=pagamentoAtual;
+            
+            System.out.println("Salário por hora do colaborador 2, horas trabalhadas do colaborador 2");
+            salario = scanner.nextDouble();
+            horas = scanner.nextDouble();
+            pagamentoAtual=salario*horas;
+            System.out.println(pagamentoAtual);
+            total+=pagamentoAtual;
+            
+            System.out.println("Salário por hora do colaborador 3, horas trabalhadas do colaborador 3");
+            salario = scanner.nextDouble();
+            horas = scanner.nextDouble();
+            pagamentoAtual=salario*horas;
+            System.out.println(pagamentoAtual);
+            
+            total+=pagamentoAtual;
+            System.out.println("Total: "+ total);
+        }
     }
 }
