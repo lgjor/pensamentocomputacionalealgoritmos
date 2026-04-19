800095f3c551d207119466d39781034f4b36c30b
lgjor <lucasgodoyjor@gmail.com>
2026-04-19 11:46:19 -0300

    Adiciona a solução do exercício 3, altura em Java

--- 05 - Java - Conceitos Básicos/03 - Altura/Altura2.java
+++ 05 - Java - Conceitos Básicos/03 - Altura/Altura2.java
@@ -4,24 +4,33 @@ public class Altura2 {
 
     public static void main(String args[]){
 
-        double alturaEmMetros;
+        double alturaEmMetros, alturaEmCM, alturaEmPolegadas;
+
+        int alturaEmPes;
 
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Informe a altura em metros: ");
-        
+
+        // Lê a altura em metros
         alturaEmMetros = scanner.nextDouble();
 
-        double alturaTotalEmPes = alturaEmMetros * 3.28084;
-            
-        double pes = Math.floor(alturaTotalEmPes);
+        // Converte a altura em Centímetros
+        alturaEmCM = alturaEmMetros*100;
 
-        double sobraEmPes = alturaTotalEmPes % 1;
+        // Calcula a altura em Polegadas
+        alturaEmPolegadas = alturaEmCM/2.54;
 
-        double polegadas = Math.round(sobraEmPes * 12);
-        
-        System.out.printf("Altura pés e polegadas: %d %d%n" , (int) pes, (int) polegadas);
+        // Calcula quantos pés existem na altura em polegadas pegando a parte inteira da divisão por 12
+        alturaEmPes = (int)(alturaEmPolegadas / 12);
 
+        // Atualiza a altura em Polegadas para ter apenas o resto da altura em pés
+        alturaEmPolegadas = Math.round(alturaEmPolegadas - alturaEmPes*12);
+
+        //Imprime o resultado
+        System.out.println(alturaEmPes + " " + (int)(alturaEmPolegadas));
+
+        // Fecha o leitor/scanner
         scanner.close();
 
     }
