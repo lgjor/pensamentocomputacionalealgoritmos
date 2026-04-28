c47066c60c78aae3addd9d2f1e64f929940edcbf
Lucas <lucasgodoyjor@gmail.com>
2026-04-28 19:35:44 -0300

    adiciona exercício 6 - vendedor e cálculo de comissão

---
+++ 06 - Java - Conceitos Básicos/05 - Vendedor/readme.md
@@ -0,0 +1,13 @@
+# Exercício em sala: Vendedor.java
+
+Considere um vendedor cuja remuneração mensal é composta por um salário fixo de R$ 2000,00 mais uma comissão que depende do valor total de suas vendas. A comissão inicial é de 5% sobre o valor total de vendas, mas se o valor total de vendas alcançar a meta de R$ 10000,00, a comissão passa a ser de 7%. Além disso, quando a avaliação de desempenho do vendedor for igual ou superior a 8, ele recebe um bônus de R$ 500,00.
+
+Elabore um programa que LÊ DO TECLADO o valor total de vendas e a avaliação de desempenho do vendedor e ESCREVE na tela o valor total da remuneração mensal.
+
+## Exemplo de execução
+
+```shell
+Entre com o total de vendas (R$): 20000
+Entre com a avaliação de desempenho (1-10): 6
+Remuneração: R$ 3400.00
+```
---
+++ 06 - Java - Conceitos Básicos/05 - Vendedor/vendedor.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+public class vendedor {
+    public static void main(String args[]){
+        final double SALARIOFIXO = 2000;
+        final double META = 10000;
+        final double BONUS = 500;
+
+        double valorVenda;
+        int avaliacaoDesempenho;
+        double remuneracaoFinal;
+        double comissao;
+        
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Entre com o valor total de vendas (R$): ");
+        valorVenda = scanner.nextDouble();
+
+        System.out.print("Entre com a avaliação de desempenho: ");
+        avaliacaoDesempenho = scanner.nextInt();
+
+        double bonus = 0;
+
+        if ( valorVenda>=META ){
+            comissao = 0.07*valorVenda;
+        }
+        else {
+            comissao = 0.05*valorVenda;
+        }
+
+        if (avaliacaoDesempenho>=8){
+            bonus = BONUS;
+        }
+
+        // CÁLCULO DA REMUNERAÇÃO FINAL
+        remuneracaoFinal = SALARIOFIXO + comissao + bonus;
+
+        System.out.printf("Remuneração: R$ %.2f\n", remuneracaoFinal);
+
+        scanner.close();
+      }    
+}
